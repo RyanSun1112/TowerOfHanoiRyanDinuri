96e45b9ecd8ddba607e0cac9434f05ff0a84113c
Dinuri <dgard1@ocdsb.ca>
2023-12-12 11:30:37 -0500

    :)

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@
 public class Main {
     public static void main(String[] args) {
         //StartPage cool = new StartPage();
-        testClassTheSecond class2 = new testClassTheSecond();
-        class2.someMethod();
+        testClassTheSecond cool = new testClassTheSecond();
+        cool.homePage();
     }
 }
--- src/testClassTheSecond.java
+++ src/testClassTheSecond.java
@@ -2,16 +2,51 @@ import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.*;
+import java.io.File;
+import java.io.IOException;
 
 public class testClassTheSecond {
+    //INSTANCE VARIABLES
     JFrame frame = new JFrame("This is the epic-est name of all time.");
+    Color white = new Color(255,255,255);
 
-    public void someMethod() {
+
+    //FORMATTING METHODS
+    private Font theNormalFont(int size) {
+
+        GraphicsEnvironment graphics = GraphicsEnvironment.getLocalGraphicsEnvironment();
+        File fontFile = new File("C:\\Users\\Dinuri\\Pictures\\Saved Pictures\\Other\\Fonts\\CaviarDreams\\CaviarDreams.ttf\\");
+        Font caviarDreams = null;
+        try {
+            caviarDreams = Font.createFont(Font.TRUETYPE_FONT,fontFile).deriveFont(Font.BOLD,size);
+            graphics.registerFont(Font.createFont(Font.TRUETYPE_FONT, fontFile));
+            return caviarDreams;
+        } catch (IOException | FontFormatException e) {
+            System.out.println("ERROR! Code in 'e.printStackTrace()' to print stack trace: ");
+        }
+        return caviarDreams;
+    }
+
+    private void formatButton(JButton button) {
+        button.setFont(theNormalFont(35));
+        button.setFocusable(false);
+        button.setBorder(BorderFactory.createEmptyBorder());
+        button.setOpaque(false);
+        button.setContentAreaFilled(false);
+        //button.setBorderPainted(false);
+        button.setForeground(white);
+
+    }
+
+
+    //PAGE METHODS
+    public void homePage() {
         JLayeredPane pane = new JLayeredPane();
         Icon backgroundIcon = new ImageIcon("C:\\Users\\Dinuri\\Downloads\\artPortfolio\\Digital\\towersOfHanoi_homePage.png");
         JLabel background = new JLabel(backgroundIcon);
 
         JButton instructionsButton = new JButton();
+        formatButton(instructionsButton);
         instructionsButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
@@ -21,6 +56,7 @@ public class testClassTheSecond {
         });
 
         JButton exit = new JButton();
+        formatButton(exit);
         exit.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
@@ -38,8 +74,8 @@ public class testClassTheSecond {
             }
         });
 
-        playButton.setBounds(885,475,376,80);
-        playButton.setBackground(new Color(255,255,255));
+        formatButton(playButton);
+        playButton.setBounds(85,575,376,80);
         pane.add(playButton);
 
         background.setBounds(0,0,1344,756);
