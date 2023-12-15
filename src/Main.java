e5f6c662b811cf5f46a7706ee10da97c7b585509
Dinuri <dgard1@ocdsb.ca>
2023-12-14 21:12:10 -0500

    :)

--- src/Main.java
+++ src/Main.java
@@ -8,10 +8,3 @@ public class Main {
 
     }
 }
-
-/*
-        First.setBounds(200, 200, 207,64);
-        Second.setBounds(200, 250, 227,64);
-        Third.setBounds(200, 300, 245,64);
-        Fourth.setBounds(200, 350, 269,64);
- */
